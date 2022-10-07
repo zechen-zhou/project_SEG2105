14541d2eec1c350f1ba2b1f4adf2eda9af0146cb
Yu-Chen Lee <ylee153@uottawa.ca>
2022-10-06 22:21:42 -0400

    register function

--- app/src/main/java/com/example/projecttraining/RegisterCook.java
+++ app/src/main/java/com/example/projecttraining/RegisterCook.java
@@ -2,24 +2,69 @@ package com.example.projecttraining;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.service.autofill.SaveRequest;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.projecttraining.databinding.FragmentRegisterCookBinding;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 
 public class RegisterCook extends Fragment {
 
+    private FragmentRegisterCookBinding binding;
+
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://mealer-dd302-default-rtdb.firebaseio.com/");
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_cook, container, false);
+        binding = FragmentRegisterCookBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView welcomeCook = binding.submit;
+        TextView backToSignIn = binding.backToSignIn;
+
+        EditText firstname = binding.firstName;
+        EditText lastname = binding.lastName;
+        EditText email = binding.email;
+        EditText password = binding.password;
+        EditText address = binding.homeAddress;
+        EditText description = binding.cookDescription;
+
+        welcomeCook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_registerCook_to_welcomeCook);
+            }
+        });
+
+        backToSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate((R.id.action_registerCook_to_login));
+            }
+        });
     }
 }
 
