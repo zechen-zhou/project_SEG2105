04d5ac4fac0f5f9489aa734380ec25d5bc5c146f
Yu-Chen Lee <ylee153@uottawa.ca>
2022-10-07 23:49:58 -0400

    registerCook connect to firebase

--- app/src/main/java/com/example/projecttraining/RegisterCook.java
+++ app/src/main/java/com/example/projecttraining/RegisterCook.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.projecttraining.databinding.FragmentRegisterCookBinding;
 import com.google.firebase.database.DatabaseReference;
@@ -42,8 +43,8 @@ public class RegisterCook extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        TextView welcomeCook = binding.submit;
-        TextView backToSignIn = binding.backToSignIn;
+        Button welcomeCook = binding.submit;
+        Button backToSignIn = binding.backToSignIn;
 
         EditText firstname = binding.firstName;
         EditText lastname = binding.lastName;
@@ -56,6 +57,26 @@ public class RegisterCook extends Fragment {
             @Override
             public void onClick(View view) {
                 Navigation.findNavController(view).navigate(R.id.action_registerCook_to_welcomeCook);
+
+                //get data from EditText from String variables
+                String firstnameText = firstname.getText().toString();
+                String lastnameText = lastname.getText().toString();
+                String emailText = email.getText().toString();
+                String passwordText = password.getText().toString();
+                String addressText = address.getText().toString();
+                String descriptionText = description.getText().toString();
+
+                // check if user fill all the fields
+                if (firstnameText.isEmpty() || lastnameText.isEmpty() ||emailText.isEmpty() || passwordText.isEmpty() || addressText.isEmpty()) {
+                    Toast.makeText(getActivity(), "Please fill up all fields", Toast.LENGTH_SHORT).show();
+                }
+
+                databaseReference.child("user").child(emailText).child("firstname").setValue(firstnameText);
+                databaseReference.child("user").child(emailText).child("lastname").setValue(lastnameText);
+                databaseReference.child("user").child(emailText).child("password").setValue(passwordText);
+                databaseReference.child("user").child(emailText).child("address").setValue(addressText);
+                databaseReference.child("user").child(emailText).child("description").setValue(descriptionText);
+
             }
         });
 
